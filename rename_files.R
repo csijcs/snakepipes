2d7340c8ba4e2b37391d56bdcfe1fdd0eef4e94d
csijcs <41136899+csijcs@users.noreply.github.com>
2020-06-12 16:08:16 +0200

    Update and rename rename_bams.R to rename_files.R

--- rename_files.R
+++ rename_files.R
@@ -3,7 +3,6 @@
 ##
 ##  Script to rename long name .bam (eg 5905_25_wz3909_TGACTTCG_S35.bam) files from GCF core to wzNUMBER.bam files (wz3909.bam).
 ##  Deploy script from the directory that your bams are in: setwd("/DATA/your.name/your_bams/")
-##  Tesa Severson, 18 May 2020
 ##
 ##############
 
@@ -24,12 +23,10 @@ for(i in 1:length(dn)){
   comp = dn[i]
   x <- unlist(strsplit(dn[i],"_"))#split name
   y <- grep("wz", x,ignore.case=TRUE) # get only 'wz' value out of split
-  z <- grep("bam", x)
   wzname <- x[[y]] # find for 'wz' name using value from split
-  ext_tmp <- x[[z]]
-  ext <- sub("^[^.]*.", "", ext_tmp)
+  ext <- sub("^[^.]*.", "", dn[i])
   newfiles <- paste(subDir,"/",wzname,".",ext, sep="") # make new names with the folder 'rename/' first
-  file.copy(from=comp, to =newfiles) # save your renamed files into the rename folder
+  file.rename(from=comp, to =newfiles) # save your renamed files into the rename folder
   tmp[i]<-print(paste(comp," saved to ", newfiles))
 }
 tt <-as.data.frame(do.call(rbind,tmp))
